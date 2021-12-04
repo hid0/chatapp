907a7c6f5aef946373e198e5be689fa34c44e98d
Faiz Hidayatulloh <faiz14hiedayatoelloh@gmail.com>
2021-12-04 13:54:23 +0700

    set routing

--- lib/app/controllers/auth_controller.dart
+++ lib/app/controllers/auth_controller.dart
@@ -1,7 +1,12 @@
+import 'package:chatapp/app/routes/app_pages.dart';
 import 'package:get/get.dart';
 
 class AuthController extends GetxController {
   // if intro skip or don't skip
   var inSkipIntro = false.obs;
   var isLoggedIn = false.obs;
+
+  void login() {
+    Get.toNamed(Routes.HOME);
+  }
 }
